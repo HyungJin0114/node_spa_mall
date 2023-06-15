2095ba70458298ff00010c1958e9f44003c44d6e
HamHyungJin <spunkup20@naver.com>
2023-06-15 11:08:35 +0900

    First Commit All files

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+const goodsRouter = require('./routes/goods.js');
+const cartsRouter = require("./routes/carts.js");
+const connect = require("./schemas");
+connect();
+
+
+//전역 미들웨어 모든코드에서 모든API에서 작용되는코드
+//app.use(express.json());
+app.use(express.json());
+app.use("/api", [goodsRouter, cartsRouter]);
+
+// app.post("/", (req,res)=>{
+//     console.log(req.body);
+
+//     res.send("기본 URI에 POST 메소드가 정상적으로 실행되었습니다.");
+// })
+
+// app.get("/", (req,res) => {
+//     console.log(req.query);
+
+//     res.status(400).json({
+//         "KeyKey" : "value 입니다.",
+//         "이름입니다." : "이름일까요?",
+//     });
+// });
+
+// app.get("/:id", (req,res) =>{
+//     console.log(req.params);
+
+//     res.send(":id URI에 정상적으로 반환됨!");
+// })
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// localhost:3000/api -> goodsRouter
+//API경로 추가해 라우터로 가는 방법
+// app.use("/api", goodsRouter);
+
+app.listen(port, () => {
+    console.log(port, '포트로 서버가 열렸어요!');
+});
---
+++ routes/carts.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+const Cart = require("../schemas/cart.js");
+const Goods = require("../schemas/goods.js");
+
+// localhost:3000/api/carts Get Method
+router.get("/carts", async (req, res) => {
+    const carts = await Cart.find({});
+    // [
+    //  {goodId, quantity}
+    //  {goodId, quantity}
+    // ];
+    const goodsIds = carts.map((cart) => {
+        return cart.goodsIds;
+    })
+    // 결괏갑 -> [2, 3, 44]  카트 안에있는 goods아이디만 가져옴
+
+    const goods = await Goods.find({ goodsId: goodsIds });
+    // Goods에 해당하는 모든정보를 가지고 올건데,
+    // 만약 goodIds 변수 안에 존재하는 값일 때에만 조회하라.
+
+    const results = carts.map((cart) => {
+        return {
+            "quantity": cart.quantity,
+            "goods": goods.find((item) => item.goodsId === cart.goodsId)
+        }
+    })
+
+    res.json({
+        "carts":results,
+    })
+
+});
+
+
+
+
+module.exports = router;
